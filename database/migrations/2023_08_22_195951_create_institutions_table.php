5c73ce8e775fd85bb56c91ac6eed977308640f73
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2023-08-22 23:34:44 +0300

    Create institution migration, factory and seeder

---
+++ database/migrations/2023_08_22_195951_create_institutions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('institutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('logo');
+            $table->string('website')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->text('description')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('institutions');
+    }
+};
