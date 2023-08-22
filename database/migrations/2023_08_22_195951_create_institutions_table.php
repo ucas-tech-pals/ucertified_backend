adccbd8c5bccd4443d1d27eebabb187f43710aea
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2023-08-22 23:38:27 +0300

    Edit institution migration

--- database/migrations/2023_08_22_195951_create_institutions_table.php
+++ database/migrations/2023_08_22_195951_create_institutions_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
         Schema::create('institutions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('password');
             $table->string('logo');
             $table->string('website')->nullable();
             $table->string('phone_number')->nullable();
             $table->text('description')->nullable(); 
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
