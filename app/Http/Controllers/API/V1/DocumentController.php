9616a6ddc0e51120ad7603961f96b8e70d732333
BreimAlaa <breim.alaa@gmail.com>
2023-08-23 18:32:28 +0300

    Create Document migration, factory, model, controller and requests

---
+++ app/Http/Controllers/API/V1/DocumentController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\Document\StoreDocumentRequest;
+use App\Http\Requests\V1\Document\UpdateDocumentRequest;
+use App\Models\Document;
+
+class DocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDocumentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Document $document)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDocumentRequest $request, Document $document)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Document $document)
+    {
+        //
+    }
+}
