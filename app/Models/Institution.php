c8430e7e4347e8014d060007abb34f4f48bbc098
BreimAlaa <breim.alaa@gmail.com>
2023-08-24 05:55:02 +0300

    Added: Defining User-Document-Institution relationships

--- app/Models/Institution.php
+++ app/Models/Institution.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -20,4 +21,8 @@ class Institution extends Authenticatable
     protected $casts = [
         'password' => 'hashed',
     ];
+    public function documents(): HasMany
+    {
+        return $this->hasMany(Document::class);
+    }
 }
