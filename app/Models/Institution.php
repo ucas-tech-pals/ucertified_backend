1e2df4d8917862f261bef3d0e3d019bc8f44da9a
BreimAlaa <breim.alaa@gmail.com>
2023-08-24 06:27:46 +0300

    Added: Eloquent Institution-User relationship

--- app/Models/Institution.php
+++ app/Models/Institution.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -24,5 +25,9 @@ class Institution extends Authenticatable
     public function documents(): HasMany
     {
         return $this->hasMany(Document::class);
+    }
+    public function users(): HasManyThrough
+    {
+        return $this->hasManyThrough(User::class, Document::class);
     }
 }
