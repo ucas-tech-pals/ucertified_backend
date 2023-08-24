475fde96f54100e965859ba2fb89c7246fb6a0b0
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2023-08-24 03:08:41 +0300

    Update auth routing system

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\API\V1\UserAuthController;
 use App\Http\Controllers\FilesController;
-use App\Http\Controllers\InstitutonAuthController;
+use App\Http\Controllers\API\V1\InstitutonAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +19,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return response()->json([
-        'user' =>auth('user')->user(),
+        'user' => auth('user')->user(),
     ]);
 })->name('user');
 
+Route::controller(InstitutonAuthController::class)->group(function () {
+    Route::post('/institution/register', 'register');
+    Route::post('/institution/login', 'login');
+});
 
+Route::controller(UserAuthController::class)->group(function () {
+    Route::post('/user/login', 'login');
+    Route::post('/user/register', 'register');
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/institution/logout', [InstitutonAuthController::class, 'logout']);
+    Route::post('/user/logout', [UserAuthController::class, 'logout']);
+});
