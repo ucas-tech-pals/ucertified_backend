7bb2530fb8f5ecf8401a39c0b812fa9e00ef06c0
BreimAlaa <breim.alaa@gmail.com>
2023-08-24 05:55:02 +0300

    Added: Defining User-Document-Institution relationships

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Document extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function institution(): BelongsTo
+    {
+        return $this->belongsTo(Institution::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2023_08_23_152931_create_documents_table.php
+++ database/migrations/2023_08_23_152931_create_documents_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Institution;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->foreignIdFor(Institution::class);
             $table->timestamps();
         });
     }
