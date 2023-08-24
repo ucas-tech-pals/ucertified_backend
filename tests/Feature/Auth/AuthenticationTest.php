e6bf08f030aad0209aff324aa84a4000d1373fff
BreimAlaa <breim.alaa@gmail.com>
2023-08-24 18:16:46 +0300

    Add institution login test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\Institution;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -33,17 +34,39 @@ class AuthenticationTest extends TestCase
                 'updated_at',
             ],
         ]);
-        // request to api/user with token
+
         $this->get(route('user'), [
             'Authorization' => 'Bearer ' . $response['access_token'],
         ])->assertStatus(200)
             ->assertJson([
-                'user' => [
+                'data' => [
                     'id' => $user->id,
                     'name' => $user->name,
                     'email' => $user->email,
                 ],
             ]);
+    }
+
+    public function test_institution_can_authenticate_using_the_login_screen()
+    {
+        $intitution = Institution::factory()->create();
+        $response = $this->post(route('institution.login'), [
+            'email' => $intitution->email,
+            'password' => 'password',
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'message',
+            'access_token',
+            'token_type',
+            'data' => [
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
     }
 
     public function test_users_can_not_authenticate_with_invalid_email(): void
@@ -52,11 +75,22 @@ class AuthenticationTest extends TestCase
 
         $response = $this->post(route('login'), [
             'email' => 'invalid-email',
-            'password' => 'password',
+            'password' => $user->password,
         ]);
         $response->assertStatus(302);
         $response->assertSessionHasErrors('email');
 
+    }
+
+    public function test_institution_can_not_authenticate_with_invalid_email() : void
+    {
+        $institution = Institution::factory()->create();
+        $response = $this->post(route('institution.login'), [
+            'email' => 'invalid-email',
+            'password' => $institution->password,
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
     }
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
@@ -64,6 +98,16 @@ class AuthenticationTest extends TestCase
 
         $response = $this->post(route('login'), [
             'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('password');
+    }
+    public function test_institution_can_not_authenticate_with_invalid_password(): void
+    {
+        $institution = Institution::factory()->create();
+        $response = $this->post(route('institution.login'), [
+            'email' => $institution->email,
             'password' => 'wrong-password',
         ]);
         $response->assertStatus(302);
