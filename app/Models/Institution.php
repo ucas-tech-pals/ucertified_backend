811f4a11c2cb6f33cf361dfabe75d87ac8f94736
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2023-08-24 03:09:13 +0300

    Make institution model authenticatable

--- app/Models/Institution.php
+++ app/Models/Institution.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Institution extends Model
+class Institution extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
