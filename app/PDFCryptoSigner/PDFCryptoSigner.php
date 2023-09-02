da7a84874848b0da1e4391e88a7bf6e54eb33b67
BreimAlaa <breim.alaa@gmail.com>
2023-09-02 10:08:40 +0300

    Signing the headers for protection

--- app/PDFCryptoSigner/PDFCryptoSigner.php
+++ app/PDFCryptoSigner/PDFCryptoSigner.php
@@ -36,76 +36,74 @@ class PDFCryptoSigner implements CryptoSigner
     public function sign(string $privateKey, array $signerInfo = []): string
     {
         $content = file_get_contents($this->path);
-        $base64File = CryptoManager::base64Encode($content);
-        $hashed = CryptoManager::hash($base64File);
-        $signed = CryptoManager::sign($hashed, $privateKey);
-
         $signerInfo['version'] = 1;
         $signerInfo['date'] = date('Y-m-d H:i:s');
 
         $headers = json_encode($signerInfo);
         $base64Headers = CryptoManager::base64Encode($headers);
-//        $encryptedHeaders = CryptoManager::encrypt($base64Headers, $privateKey);
+        $content = $content . "\n" . '% ' . '7PI' . $base64Headers;
 
-        $comment = '7PI' . $signed . '7PI' . $base64Headers;
+        file_put_contents($this->path(), $headers);
 
-        $modifiedPDFContent = $content . '\n' . '% ' . $comment;
+        $base64File = CryptoManager::base64Encode($content);
+        $hashed = CryptoManager::hash($base64File);
+        $signature = CryptoManager::sign($hashed, $privateKey);
+        $signature = CryptoManager::base64Encode($signature);
+
+        $comment = '7PI' . $signature;
+        $modifiedPDFContent = $content . $comment;
 
         file_put_contents($this->path, $modifiedPDFContent);
 
         return $comment;
     }
 
-    public function getHeaders()
-    {
-        try {
-        $content = file_get_contents($this->path);
-        $file = explode('\n', $content);
-        $signatures = end($file);
-        $signatures = substr($signatures, 2);
-
-        $explodedSignature = explode('7PI', $signatures);
-        if (count($explodedSignature) !== 3) {
-            throw new Exception('Invalid signature');
-        }
-        $encryptedHeaders = $explodedSignature[2];
-
-        return json_decode(CryptoManager::base64Decode($encryptedHeaders), true);
-
-        } catch (Exception $e) {
-            return false;
-        }
-    }
     public function verify(string $publicKey): array
     {
         try {
             $content = file_get_contents($this->path);
-            $file = explode('\n', $content);
-            $signatures = end($file);
-            $signatures = substr($signatures, 2);
-            array_pop($file);
-            $file = implode('\n', $file);
 
+            $explodedContent = explode("\n", $content);
+            $embeddedSignature = end($explodedContent);
+            array_pop($explodedContent);
+            $explodedSignature = explode('7PI', $embeddedSignature);
 
-            $base64File = CryptoManager::base64Encode($file);
-            $hashed = CryptoManager::hash($base64File);
-
-            $explodedSignature = explode('7PI', $signatures);
             if (count($explodedSignature) !== 3) {
                 throw new Exception('Invalid signature');
             }
-            $fileSignature = $explodedSignature[1];
-            $encryptedHeaders = $explodedSignature[2];
 
-            $headers['verified'] = CryptoManager::verify($hashed, $fileSignature, $publicKey);
+            $encodedHeaders = $explodedSignature[1];
+            $encodedSignature = $explodedSignature[2];
+            array_pop($explodedSignature);
+
+            $embeddedSignature = implode('7PI', $explodedSignature);
+
+            $explodedContent[] = $embeddedSignature;
+            $content = implode("\n", $explodedContent);
+
+            $base64File = CryptoManager::base64Encode($content);
+            $hashed = CryptoManager::hash($base64File);
+
+            $signature = CryptoManager::base64Decode($encodedSignature);
+            $headers['verified'] = CryptoManager::verify($hashed, $signature, $publicKey);
+
             if ($headers['verified']){
-                $headers = array_merge($headers, json_decode(CryptoManager::base64Decode($encryptedHeaders), true));
+                $headers = array_merge($headers, json_decode(CryptoManager::base64Decode($encodedHeaders), true));
             }
 
         } catch (Exception $e) {
             $headers = ['verified' => false];
         }
         return $headers;
+    }
+
+    public function getHeaders()
+    {
+        try {
+
+        } catch (Exception){
+            return;
+        }
     }
 
     public function path(): string
